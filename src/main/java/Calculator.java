3489af6d9ddb695846c011a779f20720abc2acc3
Jason Anderson <anderson.jason.az@gmail.com>
2022-01-04 11:20:05 -0700

    Finish Fibonacci

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -39,7 +39,21 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        return 0;
+        if (n == 0) {
+            return 0;
+        }
+
+        return getFibonacci(n);
+    }
+
+    private int getFibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return getFibonacci(n - 1) + getFibonacci(n - 2);
+        }
     }
 
 
