33f9197433234a88b1f51b4a1dcbb949faaea501
Jason Anderson <anderson.jason.az@gmail.com>
2022-01-05 15:53:12 -0700

    Fix bad problem

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -22,8 +22,6 @@ class Calculator {
         return a / b;
     }
 
-    Jason is super cool!!!
-
     /*
     Returns the n'th number in the fibonacci sequence
     https://en.wikipedia.org/wiki/Fibonacci_number
